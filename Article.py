e7900da33f0d20d7741003d9c65da759e1f73a22
hejunwei <hejunwei@bluestoneapp.com>
2017-08-06 18:27:56 +0800

    add search suggest

--- Article.py
+++ Article.py
@@ -1,20 +1,43 @@
-from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text
+from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text, Completion
 from elasticsearch_dsl.connections import connections
 import yaml
+from elasticsearch_dsl.analysis import CustomAnalyzer as _CustomAnalyzer
 
 with open('config.yml') as ymlfile:
     conf = yaml.load(ymlfile)
 
-#弥补es_dsl代码缺陷
-# class CustomAnalyzer(_CustomAnalyzer):
-#     def get_analysis_definition(self):
-#         return {}
-#
-#
-# ik_analyzer = CustomAnalyzer("ik_max_word",filter=['lowercase'])
+es = connections.create_connection(hosts=conf['elasticsearch'])
+
+
+class CustomAnalyzer(_CustomAnalyzer):
+    def get_analysis_definition(self):
+        return {}
+
+
+ik_analyzer = CustomAnalyzer("ik_max_word",filter=['lowercase'])
+
+def gen_suggest(index, info_tuple):
+    used_word = set()
+    suggests = []
+    for text, weight in info_tuple:
+        if text:
+            #调用es的analyze接口分析字符串
+            words = es.indices.analyze(index=index, analyzer="ik_max_word", params={'filter':['lowercase']}, body=text)
+            analyzed_words = set([r['token'] for r in words['tokens'] if len(r['token'])>1])
+            new_words = analyzed_words - used_word
+        else:
+            new_words = set()
+        if new_words:
+            suggests.append({
+                'input':list(new_words),
+                'weight':weight
+            })
+    return suggests
 
 
 class Article(DocType):
+
+    suggest = Completion(analyzer = ik_analyzer)
 
     title = Text(analyzer='ik_max_word')
     content = Text(analyzer='ik_max_word')
@@ -30,7 +53,7 @@ class Article(DocType):
         return super(Article, self).save(** kwargs)
 
     def store(self, data):
-        connections.create_connection(hosts=conf['elasticsearch'])
+
         Article.init()
         article = Article()
         article.meta['id'] = data['id']
@@ -39,4 +62,5 @@ class Article(DocType):
         article.created_at = data['created_at']
         article.content = data['content']
         article.tags = [data['tag']['type']]
+        article.suggest = gen_suggest(Article._doc_type.index, ((article.title, 10), (article.content, 7), (article.tags[0], 5)))
         article.save()
--- main.py
+++ main.py
@@ -10,8 +10,8 @@ topic = client.topics[b'posts']
 consumer = topic.get_simple_consumer()
 for message in consumer:
     if message is not None:
-        p = Process(message)
         try:
+            p = Process(message)
             p.process()
         except Exception as e:
             print(e)
